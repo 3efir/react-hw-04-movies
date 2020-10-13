9fc319d26525cbb8a201da6f7ec54aa972517a84
Oleksandr Fihun <oleksandr.fihun@globallogic.com>
2020-10-13 15:56:47 +0300

    Updates

---
+++ precache-manifest.4434df7494ef1d6e10ce0bcbfed039c1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283ce52024a58a443ee899514a241958",
+    "url": "/react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "7fe507e0bdf5ff16256c",
+    "url": "/react-hw-04-movies/static/css/main.bafa73cf.chunk.css"
+  },
+  {
+    "revision": "303a200b649df69f1bbb",
+    "url": "/react-hw-04-movies/static/css/movie-datails-page.7a7b78df.chunk.css"
+  },
+  {
+    "revision": "f38dcd4a374d7c232385",
+    "url": "/react-hw-04-movies/static/js/7.14f7305c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hw-04-movies/static/js/7.14f7305c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90773e68c0445eefa98a",
+    "url": "/react-hw-04-movies/static/js/home-page.8a694414.chunk.js"
+  },
+  {
+    "revision": "7fe507e0bdf5ff16256c",
+    "url": "/react-hw-04-movies/static/js/main.0c7564c4.chunk.js"
+  },
+  {
+    "revision": "303a200b649df69f1bbb",
+    "url": "/react-hw-04-movies/static/js/movie-datails-page.ab637cc7.chunk.js"
+  },
+  {
+    "revision": "cb2bd2eabbe1b4095195",
+    "url": "/react-hw-04-movies/static/js/movied-details-cast.e45fc760.chunk.js"
+  },
+  {
+    "revision": "e47de8bda1f7dfc42247",
+    "url": "/react-hw-04-movies/static/js/movied-details-reviews.add80025.chunk.js"
+  },
+  {
+    "revision": "5b55ad020d3b8d67e817",
+    "url": "/react-hw-04-movies/static/js/movies-page.b85668db.chunk.js"
+  },
+  {
+    "revision": "9eff6e1f87241b478ed5",
+    "url": "/react-hw-04-movies/static/js/runtime-main.1d07fc9e.js"
+  }
+]);
